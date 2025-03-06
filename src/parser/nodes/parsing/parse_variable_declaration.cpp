12d41657ffad83d463172c1b5192ad0431d51fb9
maxnut <maxnut905@gmail.com>
2025-03-07 00:00:00 +0100

    change var decl syntax

--- src/parser/nodes/parsing/parse_variable_declaration.cpp
+++ src/parser/nodes/parsing/parse_variable_declaration.cpp
@@ -4,17 +4,25 @@
 Result<std::unique_ptr<AST::Node>> Parser::parseVariableDeclaration(TokenCursor& cursor) {
     std::unique_ptr<AST::Node> ret = nullptr;
 
-    bool assignment = Rules::isAssignment(cursor);
     std::unique_ptr<AST::VariableDeclarationNode> variableDeclaration = std::make_unique<AST::VariableDeclarationNode>();
     variableDeclaration->m_rangeBegin = {cursor.get().value().m_line, cursor.get().value().m_column};
+
+    if(!expectTokenType(cursor.get().value(), Token::KEYWORD))
+        return unexpectedTokenExpectedType(cursor.value(), Token::KEYWORD);
+    if(!expectTokenValue(cursor.get().value(), "let"))
+        return unexpectedTokenExpectedValue(cursor.value(), "let");
+
+    variableDeclaration->m_name = cursor.next().get().next().value().m_value;
+
+    if(!expectTokenType(cursor.get().next().value(), Token::COLON))
+        return unexpectedTokenExpectedType(cursor.value(), Token::COLON);
 
     auto typeOpt = parseTypename(cursor);
     if(!typeOpt)
         return std::unexpected{typeOpt.error()};
 
     variableDeclaration->m_type = typeOpt.value();
-    variableDeclaration->m_name = cursor.get().next().value().m_value;
-
+    bool assignment = Rules::isAssignment(cursor);
     if(assignment) {
         cursor.next();
         auto exprOpt = parseExpression(cursor);

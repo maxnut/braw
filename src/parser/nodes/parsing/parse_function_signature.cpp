1fb98f3948af621fe93eade89cc8b9da0fdf0f70
maxnut <maxnut905@gmail.com>
2025-03-07 00:00:00 +0100

    fix

--- src/parser/nodes/parsing/parse_function_signature.cpp
+++ src/parser/nodes/parsing/parse_function_signature.cpp
@@ -17,15 +17,17 @@ Result<AST::FunctionSignature> Parser::parseFunctionSignature(TokenCursor& curso
 
     while(cursor.hasNext() && cursor.get().value().m_type != Token::RIGHT_PAREN) {
         std::unique_ptr<AST::VariableDeclarationNode> var = std::make_unique<AST::VariableDeclarationNode>();
+        
+        var->m_name = cursor.get().next().value().m_value;
+
+        if(!expectTokenType(cursor.get().next().value(), Token::COLON))
+            return unexpectedTokenExpectedType(cursor.value(), Token::COLON);
+
         Result<Identifier> typeRes = parseTypename(cursor);
         if(!typeRes)
             return std::unexpected{typeRes.error()};
         
         var->m_type = typeRes.value();
-        
-        if(cursor.get().value().m_type == Token::IDENTIFIER)
-            var->m_name = cursor.get().next().value().m_value;
-
         sig.m_parameters.push_back(std::move(var));
 
         if(cursor.get().value().m_type == Token::COMMA)
